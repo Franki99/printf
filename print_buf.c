b34d7a2096848afb1e7db728d36e01c6f311aa8e
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-05-23 10:58:04 -0700

    Files handl_buf and print_buf created

---
+++ print_buf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_buf - prints buffer
+ * @buf: buffer pointer
+ * @nbuf: number of bytes to print
+ * Return: number of bytes to print
+ */
+
+int print_buf(char *buf, unsigned int nbuf)
+{
+	return (write(1, buf, nbuf));
+}
